2cbeccf977f6513468d72c942d16a95d6241e022
pch24865 <pch24865@gmail.com>
2020-11-05 22:13:59 +0900

    AccountDeleteActivity 레이아웃 작성 (#50)

---
+++ app/src/main/java/com/mh16629/onedayonepage/account/AccountDeleteActivity.java
@@ -0,0 +1,15 @@
+package com.mh16629.onedayonepage.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.mh16629.onedayonepage.R;
+
+import android.os.Bundle;
+
+public class AccountDeleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_delete);
+    }
+}
