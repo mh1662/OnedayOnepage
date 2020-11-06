b00e1dd4724d91654909f76eda3252575d9ab099
pch24865 <pch24865@gmail.com>
2020-11-07 01:51:59 +0900

    레이아웃작성 (#52)

---
+++ app/src/main/java/com/mh16629/onedayonepage/account/AccountCleanupCompleteActivity.java
@@ -0,0 +1,16 @@
+package com.mh16629.onedayonepage.account;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mh16629.onedayonepage.R;
+
+public class AccountCleanupCompleteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_cleanup_complete);
+    }
+}
